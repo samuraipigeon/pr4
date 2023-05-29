5feb541630470abb21966e9adc43d27ae4fc2553
Ulianets_Yulia <118103514+samuraipigeon@users.noreply.github.com>
2023-05-29 22:26:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string sentence = "Якщо не можна вітер змалювати, прозорий вітер на ясному тлі, — змалюй дуби, могутні та крислаті, котрі од вітру гнуться до землі.";
+            Console.WriteLine(sentence);
+            Console.WriteLine("Введіть літеру: ");
+            string find = Console.ReadLine();
+            string[] s1 = sentence.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in s1) 
+            {
+                if(word.StartsWith(find)) {Console.Write(word+" "); }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
